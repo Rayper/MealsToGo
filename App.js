1dabd1c1d34cbc87e68767acab4dc798b63acccb
Rayper <raihangunawan22@gmail.com>
2022-03-25 14:55:45 +0700

    video 7.7 Solution App Layout

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-        <Text>Our Native Blank Page</Text>
+    <>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.searchBar}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List Restaurant</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  },
+  searchBar:  {
+    padding: 16,
+    backgroundColor: 'green'
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue'
   }
 });
