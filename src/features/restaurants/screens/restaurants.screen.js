8608b015e921f132d95644c6ac31655658d78e0f
Rayper <raihangunawan22@gmail.com>
2022-03-28 10:15:32 +0700

    video 21 resturant info card

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,31 @@
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper'
+import { RestaurantInfo } from '../components/restaurant-info.component';
+
+export const RestarantScreen = () => (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.searchBar}>
+            <Searchbar />
+        </View>
+        <View style={styles.list}>
+            <RestaurantInfo />
+        </View>
+    </SafeAreaView>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+    },
+    searchBar: {
+        padding: 16,
+        backgroundColor: '#6cb221',
+    },
+    list: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#7cce23',
+    },
+});
