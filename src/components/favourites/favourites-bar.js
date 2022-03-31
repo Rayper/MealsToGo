3f02e1228b753144403c0a27ac127a7ecfe81699
Rayper <raihangunawan22@gmail.com>
2022-03-31 18:33:59 +0700

    video 8.46 Building A Favourites Bar Part 2

--- src/components/favourites/favourites-bar.js
+++ src/components/favourites/favourites-bar.js
@@ -1,4 +1,44 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
+import styled from 'styled-components'
+import { Spacer } from '../spacer/spacer.component'
+import { CompactRestaurantInfo } from '../restaurant/compact-restaurant-info.component'
+import { Text } from '../typography/text.component'
 
-export const FavouritesBar = () => <Text>Favourites Bar</Text>;
+const FavouritesWrapper = styled.View`
+    padding: 10px;
+    background-color: #6cb221
+`;
+
+// horizontal showsHorizontalScrollIndicator={false} -> supaya melebar ke kanan
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+    return (
+        <FavouritesWrapper>
+
+            <Spacer variant="left.large">
+                <Text variant="caption">Favourites</Text>
+            </Spacer>
+
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {/* looping restorant yang jadi favourites */}
+                {favourites.map((restaurant) => {
+                    // generate key untuk masing2 resto
+                    const key = restaurant.name;
+                    return (
+                        <Spacer key={key} position="left" size="medium">
+                            <TouchableOpacity
+                                onPress={() =>
+                                    onNavigate("RestaurantDetail", {
+                                        restaurant,
+                                    })
+                                }
+                            >
+                                <CompactRestaurantInfo restaurant={restaurant} />
+                            </TouchableOpacity>
+                        </Spacer>
+                    );
+                })}
+            </ScrollView>
+        </FavouritesWrapper>
+    )
+}
