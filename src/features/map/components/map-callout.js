28e8ee748f404a2e92c80e6e2166063218b6d393
Rayper <raihangunawan22@gmail.com>
2022-04-01 09:32:22 +0700

    video 8.48 Android Bug

--- src/features/map/components/map-callout.js
+++ src/features/map/components/map-callout.js
@@ -1,13 +1,10 @@
 import React from "react";
-import styled from "styled-components/native";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
-
-const MyText = styled.Text``;
-
 
 // create props retaurants untuk dipassing di map screen
 export const MapCallout = ({ restaurant }) => (
     <CompactRestaurantInfo 
+        isMap
         restaurant={restaurant}
     />
 );
