6d02f00b556d1a09738745e2000931db0971987d
Rayper <raihangunawan22@gmail.com>
2022-04-01 09:28:16 +0700

    video 8.47 Storing Favourites with Async Storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,10 +1,35 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
     // create state untuk favourites
     const [favourites, setFavourites] = useState([]);
+
+    // function untuk store favourites restonya
+    const saveFavourites = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('@favourites', jsonValue)
+            } catch (e) {
+                console.log('error storing : ', e);
+            }
+        }
+
+    // ini buat get datanya
+    const loadFavourites = async () => {
+        try {
+        const value = await AsyncStorage.getItem('@favourites')
+        if(value !== null) {
+            // kalau ada data-nya
+            setFavourites(JSON.parse(value))
+        }
+        } catch(e) {
+            console.log('error loading : ', e);
+        }
+    }   
 
     const add = (restaurant) => {
         setFavourites([...favourites, restaurant]);
@@ -19,6 +44,16 @@ export const FavouritesContextProvider = ({ children }) => {
 
         setFavourites(newFavourites);
     }
+
+    // load dulu datanya sebelum save 
+    useEffect(() => {
+        loadFavourites();
+    }, []);
+
+    // kalau ada perubahan favourites, ini akan ke trigger
+    useEffect(() => {
+        saveFavourites(favourites);
+    }, [favourites]);
 
     return (
         <FavouritesContext.Provider
