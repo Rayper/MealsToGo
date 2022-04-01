a8769fc654f7c0dd6424b9ea5ffce724501545e3
Rayper <raihangunawan22@gmail.com>
2022-04-01 10:20:33 +0700

    video 8.52 Test Authentication Firebase

--- App.js
+++ App.js
@@ -1,18 +1,55 @@
+import React, { useState, useEffect } from 'react'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
 import 'react-native-gesture-handler';
+
 import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context"
 import { LocationContextProvider } from './src/services/location/location.context'
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 
 import { Navigation } from './src/infrastructure/navigation';
+import { theme } from "./src/infrastructure/theme";
+
+import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat';
+
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCWSGqFX8ttL3sYAi7nsjerqzyUd1V4JAo",
+  authDomain: "mealstogo-3f45c.firebaseapp.com",
+  projectId: "mealstogo-3f45c",
+  storageBucket: "mealstogo-3f45c.appspot.com",
+  messagingSenderId: "111887979094",
+  appId: "1:111887979094:web:85ce8ddd45f27137ac61f7"
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+}
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // cek apakah sudah connect dengan firebase
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("test@email.com", "test123")
+        .then((user) => {
+          console.log(user);
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      }, 2000);
+  }, []); 
+
   // untuk load si font oswald dan lato
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
