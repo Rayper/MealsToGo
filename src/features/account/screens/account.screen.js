0565e2f24959fbe12920b867f5b8c4b130b19a71
Rayper <raihangunawan22@gmail.com>
2022-04-01 13:38:28 +0700

    video 8.58 Account Screen Buttons

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button } from "react-native-paper";
+import { AccountBackground, AccountCover, AccountContainer, AuthButton } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-import { AccountBackground } from "../components/account.styles";
-
-export const AccountScreen = () => {
-    return <AccountBackground />;
+export const AccountScreen = ({ navigation }) => {
+    return ( 
+        <AccountBackground>
+            <AccountCover />
+            <AccountContainer>
+                <AuthButton 
+                    icon="account-circle"
+                    mode="contained"
+                    onPress={() => navigation.navigate("Login")} 
+                >
+                Login
+                </AuthButton>
+                
+                <Spacer size="large">
+                    <AuthButton 
+                        icon="lock-open-outline"
+                        mode="contained"
+                        onPress={() => navigation.navigate("Register")} 
+                    >
+                    Register
+                    </AuthButton>
+                </Spacer>
+            </AccountContainer>
+        </AccountBackground>
+    )
 };
